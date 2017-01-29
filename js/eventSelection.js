2d96642fe269603e5c9a868f7b78de56b6468d14
Ryan Tse <itseryan@gmail.com>
2017-01-28 23:16:44 -0500

    Commented Javascript for the app

--- js/eventSelection.js
+++ js/eventSelection.js
@@ -1,4 +1,9 @@
-function scheduleOptimization(){
+//Ryan Tse
+//McHacks 2017
+function scheduleOptimization() {
+
+//Temp Data while others work on the database
+//Will pull date/times from database and convert to Date type
   var start1 = "2013/05/29 12:30 PM";
   var end1 = "2013/05/29 01:00 PM";
   var start1 = new Date(Date.parse(start1));
@@ -17,37 +22,16 @@ function scheduleOptimization(){
   var end3 = new Date(Date.parse(end3));
   var weight3 = 6;
 
-  //var eventList = [["eventID1",start1,end1,weight1]];
+//This is the example list of events
+//Along with some variables such as list size
   var eventList = [["eventID1",start1,end1,weight1],["eventID2",start2,end2,weight2],["eventID3",start3,end3,weight3]];
   var numberOfEvents = eventList.length;
-  var optimizedEvents = [eventList[0]];
-  var lastVisited = 0;
-
-  var resultPath = []
-
-//  if (numberOfEvents == 1) {
-//    console.log(optimizedEvents);
-//  }
-//  else{
-//    for (currentEventIndex = 1; currentEventIndex < numberOfEvents; currentEventIndex++) {
-//      if ((eventList[currentEventIndex])[1] > (eventList[currentEventIndex-1])[2]) {
-//        optimizedEvents = optimizedEvents.concat([eventList[currentEventIndex]]);
-//        lastVisited = currentEventIndex;
-//      }
-//    }
-//
-//    console.log(optimizedEvents);
-//    console.log(optimizedEvents[0]);
-//    console.log(optimizedEvents[1]);
-//  }
-//  console.log(findMaxProfitRecursion(eventList, numberOfEvents));
-//  console.log(eventList);
-//  console.log(mergeSort(eventList));
-//  console.log(latestNonConflict(mergeSort(eventList),numberOfEvents));
+//Testing with the console log to debug
   console.log(findMaxWeightRecursion(mergeSort(eventList), numberOfEvents));
 }
- 
- //Merge Sort to ensure tha
+
+//Merge Sort to ensure that list of events are organized in a way where
+//First elements are the earliest and last is the latest events
 function mergeSort(eventList) {
   var numberOfEvents = eventList.length;
   if (numberOfEvents < 2){
@@ -60,7 +44,9 @@ function mergeSort(eventList) {
  
   return merge(mergeSort(left), mergeSort(right));
 }
- 
+//Merge helper function for mergeSort()
+//Uses left[0] which is the event located at the left index 0
+//[2] leads to the ending time because we are sorting from end times
 function merge(left, right) {
   var result = [];
   while (left.length && right.length) {
@@ -83,55 +69,32 @@ function merge(left, right) {
   return result;
 }
 
-//function endSort(eventList){
-//  var numberOfEvents = eventList.length;
-//  if (numberOfEvents == 1){
-//    console.log("numberOfEvents == 1");
-//    return eventList;
-//  }
-//  else {
-
-//    console.log("Before For");
-//    for (index = 1; index < (numberOfEvents); index++) {
-//      console.log("Start For");
-//      console.log(eventComparator(eventList[index-1],eventList[index]));
-//      if((eventComparator(eventList[index-1],eventList[index])) == false){
-//        var tempVariable = eventList[index-1];
-//        eventList[index-1] = eventList[index];
-//        eventList[index] = tempVariable;
-//      }
-//    }
-//    console.log("End of For");
-//    return eventList;
-//  }
-//}
-
-//function eventComparator(event1, event2){
-//  console.log(event1[2] < event2[2]);
-//  return (event1[2] < event2[2]); //Returns event True if event2 finishes later
-//}
-
+//Latest visited index where there were no schedule conflicts encountered
 function latestNonConflict(eventList, numberOfEvents) {
   var index = numberOfEvents - 1;//Index == the last item on list
   if (((eventList[index])[1]) >= ((eventList[index-1])[2])) {
-    console.log("Returning Index");
     return index;
   }
   else{
-    console.log("Returning False");
     return false;
   }
 }
 
+//This is a recursive function which returns the  weight based optimized number of events you can visit
+//This also returns the path which it had followed (these are the events you want to attend)
+//Using the helper function latestNonConflict(), it detects valid and non valid paths, only returning valid paths
 function findMaxWeightRecursion(eventList, numberOfEvents) {
-  if (numberOfEvents == 1) {//Base Case
+//Base Case
+  if (numberOfEvents == 1) {
     return [(eventList[numberOfEvents-1])[3],(eventList[numberOfEvents-1])[1]];
-  }// If only one element left in list, return that amount
-
-  var inlcudeWeight = (eventList[numberOfEvents-1])[3];//Start with base current weight
+  }
+// If only one element left in list, return that amount
+//Start with base current weight and pathing
+  var inlcudeWeight = (eventList[numberOfEvents-1])[3];
   var includedPath = [(eventList[numberOfEvents-1])[1]];
-  var lastSelected = latestNonConflict(eventList, numberOfEvents);//Index of last selected
-
+//Index of last selected
+  var lastSelected = latestNonConflict(eventList, numberOfEvents);
+//Recurse
   if (lastSelected != false){
     inlcudeWeight += findMaxWeightRecursion(eventList, lastSelected)[0];
     includedPath = includedPath.concat(findMaxWeightRecursion(eventList, lastSelected)[1]);
@@ -139,7 +102,6 @@ function findMaxWeightRecursion(eventList, numberOfEvents) {
 
   var excludeWeight = findMaxWeightRecursion(eventList, numberOfEvents-1)[0];
   var excludePath = findMaxWeightRecursion(eventList, numberOfEvents-1)[1];
-  console.log("End of a loop");
   if (Math.max(inlcudeWeight, excludeWeight) == inlcudeWeight) {
    return [Math.max(inlcudeWeight, excludeWeight), includedPath]; 
   }
@@ -147,10 +109,3 @@ function findMaxWeightRecursion(eventList, numberOfEvents) {
     return [Math.max(inlcudeWeight, excludeWeight), excludePath];
   }
 }
-
-
-//{
-//  sort(eventList, eventList+numberOfEvents, eventComparator);
-//
-//  return findMaxProfitRecursion(eventList, numberOfEvents)
-//}
